2a6a6255acbb1634da96638ebd4ba0537784d6e2
Paheal <bairagipriyanshi120@gmail.com>
2026-03-31 21:06:57 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script 3: User Input Script
+
+echo "Enter your name:"
+read name
+
+echo "Enter your favorite software:"
+read software
+
+echo "Hello $name!"
+echo "Your favorite software is $software"
+
+Enter your name:
+Priyanshi
+Enter your favorite software:
+Git
+Hello Priyanshi!
+Your favorite software is Git
